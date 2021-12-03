fa94aeb383bc1e56a6415d0c1141a271be324400
Gustavo Calderon <f.gustav.c@gmail.com>
2021-12-03 15:23:09 -0600

    se agrego Upload widget de Cloudinary

--- src/js/addcard.js
+++ src/js/addcard.js
@@ -1,6 +1,21 @@
 
-let form = document.querySelector("#add-form")
-
+let form = document.querySelector("#add-form");
+cloud();
+function cloud ()
+{
+    let myWidget = cloudinary.createUploadWidget({
+        cloudName: 'rookiesquad', 
+        uploadPreset: 'vmuljc8k'}, (error, result) => { 
+          if (!error && result && result.event === "success") { 
+            console.log('Done! Here is the image info: ', result.info); 
+          }
+        }
+      )
+      
+      document.getElementById("upload_widget").addEventListener("click", function(){
+          myWidget.open();
+        }, false);
+}
 
 form.addEventListener("submit",function(e){
 
